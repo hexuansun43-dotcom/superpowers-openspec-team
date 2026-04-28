63a8869393f6db40a5891c8ed01712bebb8e9820
hexuansun43-dotcom <hexuansun43@gmail.com>
2026-04-28 09:45:22 +0800

    2.5.1: fix doctor cwd and init OmcAdapter

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -9,7 +9,7 @@ import { configCommand } from '../commands/config.js';
 import { serveCommand } from '../commands/serve.js';
 import { doctorCommand } from '../commands/doctor.js';
 
-const VERSION = '2.5.0';
+const VERSION = '2.5.1';
 
 const program = new Command();
 
